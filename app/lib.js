c2bc2495d1f283e42a5f8abaa117e82ce984c24e
Junfeng Liu <china.liujunfeng@gmail.com>
2016-02-16 14:29:00 +0800

    Deploy to firebase and polyfill Array.prototype.find()

--- app/lib.js
+++ app/lib.js
@@ -1,5 +1,5 @@
 Array.prototype.insertSeparator = function(sep) {
-  if (this.length < 2) {
+  if (this === null || this.length < 2) {
     return this;
   } else {
     var result = new Array(this.length * 2 - 1);
@@ -11,4 +11,27 @@ Array.prototype.insertSeparator = function(sep) {
     }
     return result;
   }
+}
+
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
 }
