65587d4261398af9b63fa83d99ff1139f0538142
Junfeng Liu <china.liujunfeng@gmail.com>
2016-06-08 10:54:45 +0800

    Use webpack to generate SSR code

--- server/entry.jsx
+++ server/entry.jsx
@@ -3,13 +3,14 @@ require("babel-polyfill");
 
 import Koa from 'koa';
 import serve from 'koa-static';
+import commandLineArgs from 'command-line-args';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
 
 import fs from 'fs';
 import 'sugar';
-import routes from './app/routes';
+import routes from '../app/routes';
 
 function generatePage(content) {
   let html = fs.readFileSync('build/client/index.html', 'utf8');
@@ -24,12 +25,13 @@ app.use(async (ctx, next) => {
   try {
     await next();
   } catch (err) {
-    ctx.body = { message: err.message };
+    ctx.body = `<h1>${err.message}</h1><pre>${err.stack}</pre>`;
     ctx.status = err.status || 500;
   }
 });
 
 app.use(async (ctx, next) => {
+  let matched = true;
   match({routes, location: ctx.url}, (error, redirectLocation, renderProps) => {
     if (error) {
       ctx.status = 500;
@@ -39,12 +41,21 @@ app.use(async (ctx, next) => {
     } else if (renderProps) {
       ctx.status = 200;
       ctx.body = generatePage(renderToString(<RouterContext {...renderProps} />));
+    } else {
+      matched = false;
     }
   });
-  await next();
+  if(!matched) {
+    await next();
+  }
 });
 
 app.use(serve('build/client'));
 
-app.listen(3000);
-console.log('Listening on http://localhost:3000');
+const optionDefinitions = [
+  { name: 'port', alias: 'p', type: Number, defaultValue: 3000 }
+];
+const options = commandLineArgs(optionDefinitions);
+
+app.listen(options.port);
+console.log(`Listening on http://localhost:${options.port}`);
---
+++ webpack.server.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const packages = require('./package.json');
+
+const BUILD = process.env.BABEL_ENV = process.env.NODE_ENV;
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  server: path.join(__dirname, 'server/entry.jsx'),
+  build: path.join(__dirname, 'build/server')
+};
+
+const common = {
+  output: {
+    path: PATHS.build,
+    publicPath: '/'
+  },
+  target: 'node',
+  externals: fs.readdirSync('node_modules'),
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: [PATHS.app, PATHS.server]
+      },
+      { test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: ['file?name=img/[hash].[ext]', 'img?-minimize']
+      }
+    ]
+  }
+};
+
+const different = function(build) {
+  switch(build) {
+    case "development":
+      return {
+      };
+    case "production":
+      return {
+        entry: PATHS.server,
+        output: {
+          filename: 'entry.js',
+          libraryTarget: 'commonjs2'
+        },
+        module: {
+          loaders: [
+            {
+              test: /\.sass$/,
+              loader: ExtractTextPlugin.extract('style', ['css', 'sass?indentedSyntax=true']),
+              include: PATHS.app
+            }
+          ]
+        },
+        plugins: [
+          new webpack.DefinePlugin({
+            "process.env": {
+              NODE_ENV: JSON.stringify("production")
+            }
+          }),
+          new ExtractTextPlugin("css/[chunkhash].css"),
+          // new webpack.optimize.UglifyJsPlugin({
+          //   compress: {warnings: false}
+          // })
+        ]
+      };
+    default:
+      return {};
+  }
+}(BUILD);
+
+module.exports = merge(common, different);
