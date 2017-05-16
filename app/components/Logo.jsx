3d2d8fb44c075ade712fb0926e2f93cf8ab570d1
Junfeng <china.liujunfeng@gmail.com>
2017-05-16 18:15:23 +0800

    Fix PropTypes warnings

--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import App from './App';
 
 export default class Logo extends Component {
