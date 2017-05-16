cda87207ac6952f2002a1406634a74e412fb780e
Junfeng <china.liujunfeng@gmail.com>
2017-05-16 18:43:15 +0800

    Fix PropTypes warnings

--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export default function Logo(props) {
   const {name, link, image} = props;
